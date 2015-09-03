f6ad0c0aff43918362d40205514d17a518363012
cnosuke <shinnosuke@gmail.com>
2015-09-04 01:16:30 +0900

    Update ytenki

--- ytenki/run.sh
+++ ytenki/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 service memcached start
-cd /app && TZ='Asia/Tokyo' bundle exec unicorn -E production -c /app/unicorn.rb
+cd /app && TZ='Asia/Tokyo' DISPLAY=localhost:1.0 xvfb-run bundle exec unicorn -E production -c /app/unicorn.rb
